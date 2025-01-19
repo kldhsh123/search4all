f4e3908addf2ccdb19d5b505df3ae0e2e13ed4e4
kldhsh123 <1022140881@qq.com>
2025-01-19 19:38:28 +0800

    修改

--- web/src/app/components/footer.tsx
+++ web/src/app/components/footer.tsx
@@ -1,5 +1,5 @@
-import { Mails } from "lucide-react";
-import { FC } from "react";
+import { Mails } from "lucide-react"; // 导入Mails图标
+import { FC } from "react"; // 导入Locale对象
 import Locale from "../locales";
 export const Footer: FC = () => {
   return (
@@ -9,27 +9,27 @@ export const Footer: FC = () => {
         <div>
           <a
             className="text-blue-500 font-medium inline-flex gap-1 items-center flex-nowrap text-nowrap"
-            href="https://lepton.ai/"
+            href="https://www.myai.asia/"
           >
-            <Mails size={8} />
-            Lepton AI
+            官网
           </a>
         </div>
-        <div>if you need a performant and scalable AI cloud!</div>
-      </div>
-      <div className="flex items-center justify-center flex-wrap gap-x-4 gap-y-2 mt-2 text-zinc-400">
-        <a
-          className="hover:text-zinc-950"
-          href="https://github.com/fatwang2/search4all"
-        >
-          Github
-        </a>
-        <a
-          className="hover:text-zinc-950"
-          href="https://twitter.com/fatwang2ai"
-        >
-          Twitter
-        </a>
+        <div>
+          <a
+            className="text-blue-500 font-medium inline-flex gap-1 items-center flex-nowrap text-nowrap"
+            href="https://discord.com/invite/CKrvgCyK4y"
+          >
+            Discord群
+          </a>
+        </div>
+        <div>
+          <a
+            className="text-blue-500 font-medium inline-flex gap-1 items-center flex-nowrap text-nowrap"
+            href="https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=vFOh8aTsWPbQsv7ckU8-Rih9w-N6PFom&authKey=uzWNwmUSGD32aKVIGINOAcDrx0BC2I7uPz1mIMsM%2B5yygh8FIrDh7DMkRFQV2j4x8&noverify=0&group_code=330316577"
+          >
+            QQ群
+          </a>
+        </div>
       </div>
     </div>
   );
